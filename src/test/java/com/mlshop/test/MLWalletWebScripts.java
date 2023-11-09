71e499622b0b8983859298b60776626b70660537
Jovanie Dasian <DASIAN@MLHUILLIER.LOCAL>
2023-11-09 17:34:01 +0800

    try to tes tommorow

--- src/test/java/com/mlshop/test/MLWalletWebScripts.java
+++ src/test/java/com/mlshop/test/MLWalletWebScripts.java
@@ -4,71 +4,71 @@ import org.testng.annotations.Test;
 
 public class MLWalletWebScripts extends BaseTest{
 
-//    @Test(priority = 1)
-//    public void mlShopProfileNameAndDropDownValidation() throws Exception {
-//        mlWalletWeb.mlShopProfileNameAndDropDownValidation();
-//    }
-//    @Test(priority = 2)
-//    public void shopItemsProfilePageNavigation() throws Exception {
-//        mlWalletWeb.shopItemsProfilePageNavigation();
-//    }
-//    @Test(priority = 3)
-//    public void shopItemsPurchaseHistoryPageNavigation() throws Exception {
-//        mlWalletWeb.shopItemsPurchaseHistoryPageNavigation();
-//    }
-//    @Test(priority = 4)
-//    public  void shopItemsLogoutValidation() throws Exception {
-//        mlWalletWeb.shopItemsLogoutValidation();
-//    }
-//    @Test(priority = 5)
-//    public void shopItemsProfileSuccessfulLogin() throws Exception {
-//        mlWalletWeb.shopItemsProfileSuccessfulLogin();
-//    }
-//    @Test(priority = 6)
-//    public void shopItemsAvailableJewelryItemsValidation() throws Exception {
-//        mlWalletWeb.shopItemsAvailableJewelryItemsValidation();
-//    }
-//    @Test(priority = 7)
-//    public void shopItemsWatchItemsValidation() throws Exception {
-//        mlWalletWeb.shopItemsWatchItemsValidation();
-//    }
-//    @Test(priority = 8)
-//    public void shopItemsRingsProductTypePageNavigationValidation() throws Exception {
-//        mlWalletWeb.shopItemsRingsProductTypePageNavigationValidation();
-//    }
-//    @Test(priority = 9)
-//    public void shopItemsNecklacesAndPendantsProductTypePageNavigationValidation() throws Exception {
-//        mlWalletWeb.shopItemsNecklacesAndPendantsProductTypePageNavigationValidation();
-//    }
-//    @Test(priority = 10)
-//    public void shopItemsBraceletsAndBanglesProductTypePageNavigationValidation() throws Exception {
-//        mlWalletWeb.shopItemsBraceletsAndBanglesProductTypePageNavigationValidation();
-//    }
-//    @Test(priority = 11)
-//    public void shopItemsEarringsProductTypePageNavigationValidation() throws Exception {
-//        mlWalletWeb.shopItemsEarringsProductTypePageNavigationValidation();
-//    }
-//    @Test(priority = 12)
-//    public void shopItemsWatchProductTypePageNavigationValidation() throws Exception {
-//        mlWalletWeb.shopItemsWatchProductTypePageNavigationValidation();
-//    }
-//
-//    @Test(priority = 1)
-//    public void shopItemsRingProductPurchaseScenario() throws Exception {
-//        mlWalletWeb.shopItemsRingProductPurchaseScenario();
-//    }
     @Test(priority = 1)
+    public void mlShopProfileNameAndDropDownValidation() throws Exception {
+        mlWalletWeb.mlShopProfileNameAndDropDownValidation();
+    }
+    @Test(priority = 2)
+    public void shopItemsProfilePageNavigation() throws Exception {
+        mlWalletWeb.shopItemsProfilePageNavigation();
+    }
+    @Test(priority = 3)
+    public void shopItemsPurchaseHistoryPageNavigation() throws Exception {
+        mlWalletWeb.shopItemsPurchaseHistoryPageNavigation();
+    }
+    @Test(priority = 4)
+    public  void shopItemsLogoutValidation() throws Exception {
+        mlWalletWeb.shopItemsLogoutValidation();
+    }
+    @Test(priority = 5)
+    public void shopItemsProfileSuccessfulLogin() throws Exception {
+        mlWalletWeb.shopItemsProfileSuccessfulLogin();
+    }
+    @Test(priority = 6)
+    public void shopItemsAvailableJewelryItemsValidation() throws Exception {
+        mlWalletWeb.shopItemsAvailableJewelryItemsValidation();
+    }
+    @Test(priority = 7)
+    public void shopItemsWatchItemsValidation() throws Exception {
+        mlWalletWeb.shopItemsWatchItemsValidation();
+    }
+    @Test(priority = 8)
+    public void shopItemsRingsProductTypePageNavigationValidation() throws Exception {
+        mlWalletWeb.shopItemsRingsProductTypePageNavigationValidation();
+    }
+    @Test(priority = 9)
+    public void shopItemsNecklacesAndPendantsProductTypePageNavigationValidation() throws Exception {
+        mlWalletWeb.shopItemsNecklacesAndPendantsProductTypePageNavigationValidation();
+    }
+    @Test(priority = 10)
+    public void shopItemsBraceletsAndBanglesProductTypePageNavigationValidation() throws Exception {
+        mlWalletWeb.shopItemsBraceletsAndBanglesProductTypePageNavigationValidation();
+    }
+    @Test(priority = 11)
+    public void shopItemsEarringsProductTypePageNavigationValidation() throws Exception {
+        mlWalletWeb.shopItemsEarringsProductTypePageNavigationValidation();
+    }
+    @Test(priority = 12)
+    public void shopItemsWatchProductTypePageNavigationValidation() throws Exception {
+        mlWalletWeb.shopItemsWatchProductTypePageNavigationValidation();
+    }
+
+    @Test(priority = 13)
+    public void shopItemsRingProductPurchaseScenario() throws Exception {
+        mlWalletWeb.shopItemsRingProductPurchaseScenario();
+    }
+    @Test(priority = 14)
     public void shopItemsShippingDetailsPageUIValidation() throws Exception {
         mlWalletWeb.shopItemsShippingDetailsPageUIValidation();
     }
 
-    @Test(priority = 2)
+    @Test(priority = 15)
     public void shopItemsAddedItemInCartDeletedPopupValidationYesButton() throws Exception
     {
         mlWalletWeb.shopItemsAddedItemInCartDeletedPopupValidationYesButton();
     }
 
-    @Test(priority = 3)
+    @Test(priority = 16)
     public void shopItemsAddedItemInCartDeletedPopupValidationCancelButton() throws Exception
     {
         mlWalletWeb.shopItemsAddedItemInCartDeletedPopupValidationCancelButton();
